f845d24f387fb2ab2e644b933a7c0f977da62ae0
I Vek <ivekveku@gmail.com>
2023-05-11 18:53:29 +0200

    Adapt setup for colab

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 pin_and_add_path() {
   local module="$1"
   local commit_sha="$2"
-  local module_path="$(cd "$module" && git reset --hard "$commit_sha" && realpath . 2>&1 | tail -n 1)"
+  local output=$(cd "$module" && git reset --hard "$commit_sha" && realpath . 2>&1)
+  local module_path=$(echo "$output" | tail -n 1)
   if [ $? -eq 0 ]; then
     export PYTHONPATH="${PYTHONPATH}:${module_path}"
     printf "Added %s with commit %s to PYTHONPATH\n" "$module" "$commit_sha"
@@ -11,6 +12,8 @@ pin_and_add_path() {
     printf "Error: %s\n" "module_path"
   fi
 }
+
+
 
 # Add submodules to PYTHONPATH. Pin to specific commits.
 pin_and_add_path "zero123" "78bc429"
@@ -23,3 +26,5 @@ pip install -r requirements.txt
 
 # Patching zero123's ldm source to work with bumped lightning - don't fork or vendor for a single line of change
 (cd ./zero123/zero123/ldm/models/diffusion && patch < ../../../../../patches/ldm.patch)
+
+echo $PYTHONPATH
